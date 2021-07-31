772a87872c3de8164ce24d61b807ecfabfffa397
Rodrigue tech <rodriguet802@gmail.com>
2021-07-31 19:30:52 +0100

    Toute l'authentification

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,17 +2,60 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegisterType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RegisterController extends AbstractController
-{
-    #[Route('/register', name: 'register')]
-    public function index(): Response
+{ private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager) {
+        $this->entityManager = $entityManager;
+    }
+
+    #[Route('/inscription', name: 'register')]
+    public function index(Request $request , UserPasswordHasherInterface $encoder): Response
     {
+        $notification = null;
+        $user = new User();
+        $form = $this->createForm(RegisterType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+
+            $search_email  = $this->entityManager->getRepository(User::class)->findOneByEmail($user->getEmail());
+            if (!$search_email) {
+                $password = $encoder->hashPassword($user,$user->getPassword());
+                $user->setPassword($password);
+                $this->entityManager->persist($user);
+                $this->entityManager->flush();
+
+                unset($entity);
+                unset($form);
+
+                $user = new User();
+                $form = $this->createForm(RegisterType::class, $user);
+
+                $notification = "Votre inscription s'est correctement déroulée. Vous pouvez dès à présent vous connecter à votre compte.";
+            } else {
+                unset($entity);
+                unset($form);
+
+                $user = new User();
+                $form = $this->createForm(RegisterType::class, $user);
+                $notification = "L'identifiant et l'email  que vous avez renseigné existe déjà";
+            }
+        }
+
         return $this->render('register/index.html.twig', [
-            'controller_name' => 'RegisterController',
+            'form' => $form->createView(),
+            'notification' => $notification
         ]);
     }
 }
--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -4,6 +4,11 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,11 +17,51 @@ class RegisterType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
-            ->add('nom')
-            ->add('prenom')
+            ->add('nom',TextType::class,[
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Votre nom',
+                ]
+            ])
+            ->add('prenom',TextType::class,[
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Votre prénom',
+                ]
+            ])
+            ->add('email', EmailType::class,[
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Votre email',
+                    ]
+            ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Le mot de passe et la confirmation doivent être identique.',
+                'label' => 'Votre mot de passe',
+                'required' => true,
+                'first_options' => [
+                    'label' => false,
+                    'attr' => [
+                        'placeholder' => 'Votre mot de passe.',
+                        'class' => 'appearance-none block w-full bg-gray-200 text-gray-700 border border-blue-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white'
+                    ]
+                ],
+                'second_options' => [
+                    'label' => 'Confirmez votre mot de passe',
+                    'attr' => [
+                        'placeholder' => 'Confirmer votre mot de passe.',
+                        'class' => 'appearance-none block w-full bg-gray-200 text-gray-700 border border-blue-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white'
+                    ]
+                ]
+            ])
+
+            ->add('submit', SubmitType::class, [
+                'label' => "S'inscrire  maintenant",
+                'attr' => [
+                    'class' => 'inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+                ]
+            ])
         ;
     }
 
